614b6e3423a10dbc5a5b9733fbc7d8cead94b34a
ThibaultAnd261 <thibaultandwork@gmail.com>
2022-10-22 14:19:12 +0200

    début projet (ajout client id/secret)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Credentials from './Credentials';
 
 function App() {
+  const credits = Credentials();
+  //console.log(credits.REACT_APP_ClientId);
+  //console.log(process.env.REACT_APP_CLIENT_ID);
+  console.log(credits)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar />
   );
 }
 
